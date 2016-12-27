28f77de84fa04baaa7e22aef7873c05ea07677b3
00 <a00@00demacbook-pro.local>
2016-12-27 05:08:40 -0800

    Initial Commit

---
+++ FirstTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstTest
+//
+//  Created by 00 on 16/12/27.
+//  Copyright © 2016年 WangPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstTest
+//
+//  Created by 00 on 16/12/27.
+//  Copyright © 2016年 WangPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
